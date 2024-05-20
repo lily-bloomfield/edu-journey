4d197373b153465732c21879641199ad4c5972c3
lily-bloomfield <lilybloomfield.lb@gmail.com>
2024-05-20 14:11:05 +0630

    home-body-photo-change

--- our-journey/src/components/Home/HomeBody/HomeBody.jsx
+++ our-journey/src/components/Home/HomeBody/HomeBody.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import bodyImg1 from "../../../assets/home-body-img1.jpg";
-import bodyImg2 from "../../../assets/home-body-img-2.jpg";
+import bodyImg1 from "../../../assets/home-body-img2.svg";
+import bodyImg2 from "../../../assets/home-body-img1.webp";
 
 import "./HomeBody.css";
 const HomeBody = () => {
@@ -20,8 +20,8 @@ const HomeBody = () => {
           <img src={bodyImg2} />
         </div>
         <div className="homebody-2-right">
-          <h2>In bite sized steps</h2>
-          <p>Share the life events that made up your journey.</p>
+          <h2>Sharing Your Journey to Guide Others</h2>
+          <p>Write your journey so you can help others on theirs.</p>
         </div>
       </div>
     </div>
